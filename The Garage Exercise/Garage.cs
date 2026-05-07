fd01d51f6e87112d1d74d5ca7e591f6958e8c41a
Jonatan Streith <jonatan@streith.se>
2026-05-07 16:57:30 +0200

    ParkVehicle() method created.

--- The Garage Exercise/Garage.cs
+++ The Garage Exercise/Garage.cs
@@ -25,12 +25,31 @@ namespace The_Garage_Exercise
             {
                 if (vehicles[i] is null)
                 {
-                    spotFound=true;
+                    spotFound = true;
                     return i;
                 }
             }
             return -1;
             //Vehicle spot = System.Array.Find(vehicles, p => p is null);
+        }
+
+        public void parkVehicle(Vehicle vehicle)
+        {
+            int parkingSpot = findEmptySpot(out bool success);
+
+            if (success)
+            {
+                Console.WriteLine($"Parking spot {parkingSpot} is available.");
+                vehicles[parkingSpot] = vehicle;
+                Console.WriteLine($"Your {vehicle.GetType}, license number {vehicle.License}, " +
+                    $"has been parked in spot {parkingSpot}. Enjoy your stay.");
+            }
+            else
+            {
+                Console.WriteLine("Apologies, but there are no free parking spots avilable currently. " +
+                    "Please try another garage.");
+            }
+            //Possibly return the message as a string?
         }
 
     }
