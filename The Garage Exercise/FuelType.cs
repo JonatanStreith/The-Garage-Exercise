b9b142626b17788cbd6baecf3ade3f7ea977533e
Jonatan Streith <jonatan@streith.se>
2026-05-07 16:15:28 +0200

    Created enums to track fuel types and mobility types.

---
+++ The Garage Exercise/FuelType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise
+{
+    internal enum FuelType
+    {
+        Gasoline,
+        Diesel
+    }
+}
---
+++ The Garage Exercise/Mobility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise
+{
+    internal enum Mobility
+    {
+        Ground,
+        Water,
+        Air
+    }
+}
