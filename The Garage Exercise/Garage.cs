a15fc0c3eb0020e97fdd41bd5d427588e7fd250b
Jonatan Streith <jonatan@streith.se>
2026-05-07 16:43:38 +0200

    implemented Vehicle array and a constructor, as well as a method to find an empty spot (if available).

--- The Garage Exercise/Garage.cs
+++ The Garage Exercise/Garage.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using The_Garage_Exercise.Vehicles;
 
 namespace The_Garage_Exercise
 {
     internal class Garage
     {
+
+        Vehicle[] vehicles;
+
+        public Garage(int parkingSpots)
+        {
+            vehicles = new Vehicle[parkingSpots];
+        }
+
+        public int findEmptySpot(out bool spotFound)        //Returns a spot number, and a bool confirming the find
+        {
+            int emptySpot;
+            spotFound = false;
+
+            for (int i = 0; i < vehicles.Length; i++)
+
+            {
+                if (vehicles[i] is null)
+                {
+                    spotFound=true;
+                    return i;
+                }
+            }
+            return -1;
+            //Vehicle spot = System.Array.Find(vehicles, p => p is null);
+        }
+
     }
 }
