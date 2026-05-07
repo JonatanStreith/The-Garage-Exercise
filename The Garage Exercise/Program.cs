ab3b2e43d63e25e7e357bbadcefcb928d15ade54
Jonatan Streith <jonatan@streith.se>
2026-05-07 13:42:24 +0200

    Add project files.

---
+++ The Garage Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Garage_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
