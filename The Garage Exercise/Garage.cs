ec1ffbf942e39971c132d348b03f8ead64ed098a
Jonatan Streith <jonatan@streith.se>
2026-05-08 10:07:58 +0200

    Started on the garage instantiation.

--- The Garage Exercise/Garage.cs
+++ The Garage Exercise/Garage.cs
@@ -72,7 +72,7 @@ namespace The_Garage_Exercise
 
         }
 
-        public void PopulateGarage()
+        public void PopulateGarage(int number)
         {
 
         }
--- The Garage Exercise/Program.cs
+++ The Garage Exercise/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size;
+            bool success;
+            Console.Write("Welcome! Please specify size of garage (at least 5 is recommended): ");
+
+            do
+            {
+                success = int.TryParse(Console.ReadLine(), out size);
+                if(!success) Console.Write("That is not a legitimate number." +
+                    "\nPlease specify size of garage: ");
+            } while (!success);
+
+            Garage garage = new(size);
+
+            Console.WriteLine("\nA new garage has been erected.");
+            Console.Write("Would you like to populate the garage with preexisting vehicles? (Y/N) [N]\n");
+
+            string populateOrNot = Console.ReadLine().ToLower();
+
+            switch (populateOrNot) 
+            {
+                case "y":
+                    {
+                        Console.WriteLine("Okay, the garage will be populated with five vehicles " +
+                            "\n(or fewer depending on size).");
+                        garage.PopulateGarage((size<5) ? size : 5);
+                        break;
+                    }
+
+                case "n":
+                default:
+                    {
+                        Console.WriteLine("This garage will be empty from the start.");
+                        break; 
+                    }
+            
+            }
+             
         }
     }
 }
