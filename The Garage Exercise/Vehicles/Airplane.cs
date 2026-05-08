6f2c0a8f8fd0933a96f6b2de0038b29ef47e3f0d
Jonatan Streith <jonatan@streith.se>
2026-05-08 14:31:18 +0200

    Created hierarchical methods for registering a new vehicle.

--- The Garage Exercise/Vehicles/Airplane.cs
+++ The Garage Exercise/Vehicles/Airplane.cs
@@ -12,7 +12,9 @@ namespace The_Garage_Exercise.Vehicles
         private int _numberOfSeats;
         private int _length;
 
-        public Airplane(string license, string owner, string color, int wheels, Mobility mobility, int numberOfEngines, int cylinderVolume, FuelType fuelType, int numberOfSeats, int length) : base(license, owner, color, wheels, mobility)
+        public Airplane(string license, string owner, string color, int wheels, Mobility mobility,
+            int numberOfEngines, int cylinderVolume, FuelType fuelType, int numberOfSeats, int length)
+            : base(license, owner, color, wheels, mobility)
         {
             NumberOfEngines = numberOfEngines;
             CylinderVolume = cylinderVolume;
@@ -26,6 +28,39 @@ namespace The_Garage_Exercise.Vehicles
         public FuelType FuelType { get { return _fuelType; } set { _fuelType = value; } }
         public int NumberOfSeats { get { return _numberOfSeats; } set { _numberOfSeats = value; } }
         public int Length { get { return _length; } set { _length = value; } }
+
+
+
+        public static Airplane RegisterVehicle()
+        {
+
+            Console.Write("Please provide the name of the owner and the license number: ");
+            string[] ownership = Console.ReadLine().Split(",");
+
+            Console.WriteLine("Please specify the color, number of wheels (number), " +
+                "\nand mobility type (land, air, or water).");
+            string[] generalInfo = Console.ReadLine().Split(",");
+
+            Console.WriteLine("Please specify technical details: " +
+                "\nNumber of engines (number), cylinder volume (number), " +
+                "\nfuel type (gasoline or dieslel) number of seats (number)" +
+                "\nand length (number in centimeters).");
+            string[] typeInfo = Console.ReadLine().Split(",");
+
+            Console.WriteLine("Thank you. Generating registry information...");
+
+            Airplane airplane = new Airplane(ownership[1], ownership[0],
+                generalInfo[0], generalInfo[1], generalInfo[2],
+                typeInfo[0], typeInfo[1], typeInfo[2], typeInfo[3], typeInfo[3]
+                );
+
+
+
+
+            return null;
+
+
+        }
 
     }
 }
--- The Garage Exercise/Vehicles/Vehicle.cs
+++ The Garage Exercise/Vehicles/Vehicle.cs
@@ -27,5 +27,80 @@ namespace The_Garage_Exercise.Vehicles
             Mobility = mobility;
         }
 
+
+        public static Vehicle RegisterVehicle()
+        {
+            Console.WriteLine("You are required to provide the specifics of your vehicle for the registry." +
+                            "\nMultiple inputs must be separated by a comma. Some inputs may be specific types." +
+                            "\nFailure to comply may result in failed registration, denied parking, and destruction of vehicle." +
+                            "\nAll information is confidential and will be sold to the highest bidder." +
+                            "\nPlease ignore the previous sentence." +
+                            "\n");
+
+
+            Vehicle? vehicle;
+
+            Console.WriteLine("Please specify your type of vehicle." +
+                "\nAvailable options are: Car, bus, motorcycle, bicycle, boat, and airplane.");
+            Console.Write("Vehicle type: ");
+
+            string type = Console.ReadLine().ToLower();
+
+            switch (type)
+            {
+                case "car":
+                    {
+                        Console.WriteLine("Your vehicle is identified as a car.");
+                        vehicle = Car.RegisterVehicle();
+                        break;
+                    }
+
+                case "bus":
+                    {
+                        Console.WriteLine("Your vehicle is identified as a bus.");
+                        vehicle = Bus.RegisterVehicle();
+                        break;
+                    }
+
+                case "motorcycle":
+                    {
+                        Console.WriteLine("Your vehicle is identified as a motorcycle.");
+                        vehicle = Motorcycle.RegisterVehicle();
+                        break;
+                    }
+
+                case "bicycle":
+                    {
+                        Console.WriteLine("Your vehicle is identified as a bicycle.");
+                        vehicle = Bicycle.RegisterVehicle();
+                        break;
+                    }
+
+                case "boat":
+                    {
+                        Console.WriteLine("Your vehicle is identified as a boat.");
+                        vehicle = Boat.RegisterVehicle();
+                        break;
+                    }
+
+                case "airplane":
+                    {
+                        Console.WriteLine("Your vehicle is identified as an airplane.");
+                        vehicle = Airplane.RegisterVehicle();
+                        break;
+                    }
+
+
+                default:
+                    {
+                        Console.WriteLine("That is not an applicable vehicle type.");
+                        vehicle = null;
+                        break;
+                    }
+            }
+
+            return null;
+
+        }
     }
 }
