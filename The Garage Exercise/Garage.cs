dd0aa202dcf7e4c165ab6d1d12c083cb834c9a74
Jonatan Streith <jonatan@streith.se>
2026-05-11 13:41:07 +0200

    Implemented methods to park and retrieve a vehicle.

--- The Garage Exercise/Garage.cs
+++ The Garage Exercise/Garage.cs
@@ -13,7 +13,7 @@ namespace The_Garage_Exercise
         public Garage(int numberOfParkingSpots)
         {
             vehicles = new Vehicle[numberOfParkingSpots];
-            this.numberOfParkingSpots =numberOfParkingSpots;
+            this.numberOfParkingSpots = numberOfParkingSpots;
         }
 
         public int FindEmptySpot(out bool spotFound)        //Returns a spot number, and a bool confirming the find
@@ -52,27 +52,57 @@ namespace The_Garage_Exercise
             if (success)
             {
                 Console.WriteLine($"Parking spot {parkingSpot} is available.");
-                vehicles[parkingSpot] = vehicle;
-                Console.WriteLine($"Your {vehicle.GetType}, license number {vehicle.License}, " +
-                    $"has been parked in spot {parkingSpot}. Enjoy your stay.");
+
+                bool duplicate = (FindVehicleByLicense(vehicle.License) != null);
+                //If we attempt to find a vehicle with the same license number and
+                //it doesn't return null, we have a duplicate.
+
+                if (duplicate)
+                {
+                    Console.WriteLine("Illegitimate license number. " +
+                        "\nA vehicle with the same number already exist in the garage. " +
+                        "\nPolice has been alerted.");
+                }
+                else
+                {
+                    vehicles[parkingSpot] = vehicle;
+                    Console.WriteLine($"Your {vehicle.GetType}, license number {vehicle.License}, " +
+                        $"has been parked in spot {parkingSpot}. Enjoy your stay.");
+                }
+
             }
             else
             {
                 Console.WriteLine("Apologies, but there are no free parking spots avilable currently. " +
-                    "Please try another garage.");
+                    "\nPlease try another garage.");
             }
             //Possibly return the message as a string?
         }
 
         public void RetrieveVehicle()
         {
+            Console.Write("Please provide the license number of the vehicle you wish to retrieve: ");
+            string license = Console.ReadLine();
 
+            bool vehicleFound = false;
+
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                if (vehicles[i].License == license)
+                {
+                    vehicles[i] = null;
+                    vehicleFound = true;
+                    break;
+                }
+            }
+
+            if (vehicleFound == false) {
+                Console.WriteLine("No vehicle with that number exists in the garage." +
+    "\nDid you input the license number correctly?");
+
+            }
         }
 
-        public void RetrieveVehicle(Vehicle vehicle)
-        {
-
-        }
 
         public void RemoveVehicle(Vehicle vehicle)
         {
@@ -101,5 +131,19 @@ namespace The_Garage_Exercise
             Console.WriteLine("Garage is being populated, probably.");
         }
 
+
+        public Vehicle FindVehicleByLicense(string license)
+        {
+            foreach (Vehicle vehicle in vehicles)
+            {
+                if (
+                    (vehicle != null) &&
+                    (vehicle.License.Equals(license))
+                    )
+                { return vehicle; }
+            }
+
+            return null;
+        }
     }
 }
