df4d7a746b9bf728a208e77530449968522b639a
Jonatan Streith <jonatan@streith.se>
2026-05-13 17:27:22 +0200

    Created enum VehicleTypes so you can validate vehicle types.

---
+++ The Garage Exercise/VehicleTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise
+{
+    public enum VehicleTypes
+    {
+        car,
+        bus,
+        motorcycle,
+        bicycle,
+        boat,
+        airplane
+    }
+}
