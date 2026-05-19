ab363ec27509311a302701c9e5aae188be2ca149
Jonatan Streith <jonatan@streith.se>
2026-05-19 10:24:44 +0200

    Created a struct for exporting combined filter data.

---
+++ The Garage Exercise/FilterData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise
+{
+    internal struct FilterData
+    {
+        internal FilterData(string? typeFilter, string? mobilityFilter, string? colorFilter, int? wheelsFilter)
+        {
+            TypeFilter = typeFilter;
+            MobilityFilter = mobilityFilter;
+            ColorFilter = colorFilter;
+            WheelsFilter = wheelsFilter;
+        }
+
+        internal string TypeFilter { get; }
+        internal string MobilityFilter { get; }
+        internal string ColorFilter { get; }
+        internal int WheelsFilter { get; }
+
+
+    }
+}
