1d3da76a0941234bda602cb1dfe4c13eac53e086
Jonatan Streith <jonatan@streith.se>
2026-05-19 13:20:32 +0200

    Added "Multi filter vehicles" to menu.

--- The Garage Exercise/Menu.cs
+++ The Garage Exercise/Menu.cs
@@ -20,6 +20,7 @@ namespace The_Garage_Exercise
             "\n(R)etrieve vehicle" +
             "\n(L)ist vehicles" +
             "\n(F)ilter vehicles" +
+            "\n(M)ulti filter vehicles" +
             "\n(Q)uit and leave the garage" +
             "\n";
         public static void DisplayMenu()
@@ -64,6 +65,13 @@ namespace The_Garage_Exercise
                     case "Filter":
                         {
                             garageHandler.FilterVehicle();
+                            break;
+                        }
+
+                    case "M":
+                    case "Multi":
+                        {
+                            garageHandler.MultiFilterVehicle();
                             break;
                         }
 
