55a740a473719391486421b2adee7a8e5eea0a3b
Jonatan Streith <jonatan@streith.se>
2026-05-20 10:29:09 +0200

    Extracted all messages to separate strings for better oversight. Also cleanup.

--- The Garage Exercise/Managers/VehicleRegistrationManager.cs
+++ The Garage Exercise/Managers/VehicleRegistrationManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using The_Garage_Exercise.Enums;
+using The_Garage_Exercise.Tools;
 using The_Garage_Exercise.Vehicles;
 
 namespace The_Garage_Exercise.Managers
@@ -11,33 +12,57 @@ namespace The_Garage_Exercise.Managers
 
         public static Vehicle RegisterVehicle()
         {
-            Console.WriteLine("\nYou are required to provide the specifics of your vehicle for the registry." +
+            string registerMessage = "\nYou are required to provide the specifics of your vehicle for the registry." +
                             "\nMultiple inputs must be separated by a comma. Some inputs may be specific types." +
                             "\nFailure to comply may result in failed registration, denied parking, and destruction of vehicle." +
                             "\nAll information is confidential and will be sold to the highest bidder." +
-                            "\nPlease ignore the previous sentence.");
+                            "\nPlease ignore the previous sentence.";
+            string specTypePrompt = "\nPlease specify your type of vehicle." +
+                "\nAvailable options are: Car, bus, motorcycle, bicycle, boat, and airplane.";
 
+            string ownershipPrompt = "Please provide the name of the owner and the license number:";
+            string generalInfoPrompt = "Please specify the color, number of wheels (number), " +
+                "\nand mobility type (land, air, or water).";
+
+            string carTypePrompt = "Please specify technical details: " +
+                "\nCylinder volume (number), " +
+                "\nfuel type (gasoline or diesel) and number of seats (number).";
+            string busTypePrompt = "Please specify technical details: " +
+                            "\nCylinder volume (number), " +
+                            "\nfuel type (gasoline or diesel), number of seats (number)" +
+                            "\nand length (number in centimeters).";
+            string airplaneTypePrompt = "Please specify technical details: " +
+                        "\nNumber of engines (number), cylinder volume (number), " +
+                        "\nfuel type (gasoline or diesel), number of seats (number)" +
+                        "\nand length (number in centimeters).";
+            string boatTypePrompt = "Please specify technical details: " +
+                        "\nCylinder volume (number), fuel type (gasoline or diesel) " +
+                        "\nand length (number in centimeters).";
+            string motorcycleTypePrompt = "Please specify technical details: " +
+                        "\nCylinder volume (number) and fuel type (gasoline or diesel).";
+            string bicycleTypePrompt = "Please specify technical details: " +
+                        "\nNumber of  seats (number).";
+
+            string invalidType = "That is not an applicable vehicle type.";
+            string defaultError = "Something went wrong.";
+
+            Console.WriteLine(registerMessage);
 
             Vehicle? vehicle;
 
-            Console.WriteLine("\nPlease specify your type of vehicle." +
-                "\nAvailable options are: Car, bus, motorcycle, bicycle, boat, and airplane.");
-            Console.Write("Vehicle type: ");
-
-            string type = Console.ReadLine().ToLower();
+            string type = Helper.GetInput(specTypePrompt);
 
             if (!Enum.IsDefined(typeof(VehicleTypes), type))
             {
-                Console.WriteLine("That is not an applicable vehicle type.");
+                Console.WriteLine(invalidType);
                 return null;
             }
 
-            Console.Write("Please provide the name of the owner and the license number: ");
-            string[] ownership = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+            string[] ownership = Helper.GetInput(ownershipPrompt)
+                .Split(",").Select(x => x.Trim()).ToArray();
 
-            Console.WriteLine("Please specify the color, number of wheels (number), " +
-                "\nand mobility type (land, air, or water).");
-            string[] generalInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+            string[] generalInfo = Helper.GetInput(generalInfoPrompt)
+                .Split(",").Select(x => x.Trim()).ToArray();
 
             string[] typeInfo;
 
@@ -45,73 +70,52 @@ namespace The_Garage_Exercise.Managers
             {
                 case "car":
                     {
-                        Console.WriteLine("Please specify technical details: " +
-                            "\nCylinder volume (number), " +
-                            "\nfuel type (gasoline or diesel) and number of seats (number).");
-                        typeInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+                        typeInfo = Helper.GetInput(carTypePrompt)
+                            .Split(",").Select(x => x.Trim()).ToArray();
 
                         return Car.RegisterVehicle(ownership, generalInfo, typeInfo);
                     }
 
                 case "bus":
                     {
-                        Console.WriteLine("Please specify technical details: " +
-                            "\nCylinder volume (number), " +
-                            "\nfuel type (gasoline or diesel), number of seats (number)" +
-                            "\nand length (number in centimeters).");
-                        typeInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+                        typeInfo = Helper.GetInput(busTypePrompt).Split(",").Select(x => x.Trim()).ToArray();
 
                         return Bus.RegisterVehicle(ownership, generalInfo, typeInfo);
                     }
 
                 case "motorcycle":
                     {
-                        Console.WriteLine("Please specify technical details: " +
-                            "\nCylinder volume (number) and fuel type (gasoline or diesel).");
-                        typeInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+                        typeInfo = Helper.GetInput(motorcycleTypePrompt).Split(",").Select(x => x.Trim()).ToArray();
 
                         return Motorcycle.RegisterVehicle(ownership, generalInfo, typeInfo);
                     }
 
                 case "bicycle":
                     {
-                        Console.WriteLine("Please specify technical details: " +
-                            "\nNumber of  seats (number).");
-                        typeInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+                        typeInfo = Helper.GetInput(bicycleTypePrompt).Split(",").Select(x => x.Trim()).ToArray();
 
                         return Bicycle.RegisterVehicle(ownership, generalInfo, typeInfo);
                     }
 
                 case "boat":
                     {
-                        Console.WriteLine("Please specify technical details: " +
-                            "\nCylinder volume (number), fuel type (gasoline or diesel) " +
-                            "\nand length (number in centimeters).");
-                        typeInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+                        typeInfo = Helper.GetInput(boatTypePrompt).Split(",").Select(x => x.Trim()).ToArray();
 
                         return Boat.RegisterVehicle(ownership, generalInfo, typeInfo);
                     }
 
                 case "airplane":
                     {
-                        Console.WriteLine("Please specify technical details: " +
-                            "\nNumber of engines (number), cylinder volume (number), " +
-                            "\nfuel type (gasoline or diesel), number of seats (number)" +
-                            "\nand length (number in centimeters).");
-                        typeInfo = Console.ReadLine().Split(",").Select(x => x.Trim()).ToArray();
+                        typeInfo = Helper.GetInput(airplaneTypePrompt).Split(",").Select(x => x.Trim()).ToArray();
 
                         return Airplane.RegisterVehicle(ownership, generalInfo, typeInfo);
                     }
-
                 default:
                     {
-                        Console.WriteLine("Something went wrong.");
+                        Console.WriteLine(defaultError);
                         return null;
-                        break;
                     }
             }
-
-            return vehicle;
         }
     }
 }
