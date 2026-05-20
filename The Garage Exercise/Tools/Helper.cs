41720f1813727508795740162a0205b3773ce12b
Jonatan Streith <jonatan@streith.se>
2026-05-20 08:28:26 +0200

    Added Helper class for useful tools.

---
+++ The Garage Exercise/Tools/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise.Tools
+{
+    public class Helper
+    {
+
+        public static string GetInput(string message)
+        {
+            Console.WriteLine(message);
+
+            return Console.ReadLine().ToLower();
+        }
+
+
+    }
+}
