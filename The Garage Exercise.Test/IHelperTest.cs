c6241a24601232909e1ff36febf4489fc0c5c46b
Jonatan Streith <jonatan@streith.se>
2026-05-26 10:29:05 +0200

    tested MultiTesthelper to ensure it will return each string separately.

--- The Garage Exercise.Test/IHelperTest.cs
+++ The Garage Exercise.Test/IHelperTest.cs
@@ -23,5 +23,33 @@ namespace The_Garage_Exercise.Test
             Assert.Equal(testmessage, result2);
 
         }
+
+        [Fact]
+        public void MultiTestHelperCanReturnMultiSpoofedData()
+        {
+            IHelper help = new MultiTestHelper();
+            string testmessage = "Car;Steve 567ABC;red 4 land;2000 diesel 5";
+
+            help.Output = testmessage;
+
+            string result1 = help.GetInput("This is an input with a message.");
+
+            string result2 = help.GetInput();
+
+            string result3 = help.GetInput();
+
+            string result4 = help.GetInput();
+
+            string result5 = help.GetInput();
+
+            Assert.Equal("Car", result1);
+            Assert.Equal("Steve 567ABC", result2);
+            Assert.Equal("red 4 land", result3);
+            Assert.Equal("2000 diesel 5", result4);
+            Assert.Equal("Car", result5);
+
+        }
+
+
     }
 }
