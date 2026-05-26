6eb4e0e0fc8d5a1d4c324956d7623900e09d8283
Jonatan Streith <jonatan@streith.se>
2026-05-26 08:57:28 +0200

    made a test to ascertain that the spoof class I made to help testing worked properly.

---
+++ The Garage Exercise.Test/IHelperTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using The_Garage_Exercise.Tools;
+
+namespace The_Garage_Exercise.Test
+{
+    public class IHelperTest
+    {
+        [Fact]
+        public void TestHelperCanReturnSpoofedData()
+        {
+            TestHelper help = new TestHelper();
+            string testmessage = "Sphinx of black quartz, judge my vow";
+
+            help.Output = testmessage;
+
+            string result1 = help.GetInput("This is an input with a message.");
+
+            string result2 = help.GetInput();
+
+            Assert.Equal(testmessage, result1);
+            Assert.Equal(testmessage, result2);
+
+        }
+    }
+}
