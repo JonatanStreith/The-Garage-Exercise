9767e506c82a56f79df739c64c5774c3c8ffea38
Jonatan Streith <jonatan@streith.se>
2026-05-26 10:20:39 +0200

    Created a Helper method that will use an array of strings and hand out each one in turn.

---
+++ The Garage Exercise/Tools/MultiTestHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise.Tools
+{
+    internal class MultiTestHelper : IHelper
+    {
+
+        private string[] _content;
+        private int counter = -1;
+        public string? Output { 
+            get {
+                counter++;
+                if (counter >= _content.Length) counter = 0;
+                return _content[counter];
+            }
+            set { 
+                _content = value.Split(";"); 
+            } 
+        }
+
+        public string GetInput(string message)
+        {
+            Console.WriteLine(message);
+
+            return Output;
+        }
+
+        public string GetInput()
+        {
+            return Output;
+        }
+    }
+}
