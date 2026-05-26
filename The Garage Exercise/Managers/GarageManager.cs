28fae18aaf2083378e2acb5a24f7100538c30f56
Jonatan Streith <jonatan@streith.se>
2026-05-26 09:30:50 +0200

    made Manager methods return a string to confirm that the input was received correctly."

--- The Garage Exercise/Managers/GarageManager.cs
+++ The Garage Exercise/Managers/GarageManager.cs
@@ -72,7 +72,7 @@ namespace The_Garage_Exercise.Managers
             }
         }
 
-        internal void ListParkedVehicles()
+        internal string ListParkedVehicles()
         {
             string listPrompt = "\nPlease specify category of vehicle (or all): ";
 
@@ -95,9 +95,10 @@ namespace The_Garage_Exercise.Managers
             else
                 Console.WriteLine($"Total {results.Count} {input}s.");
 
+            return $"Listed {results.Count} '{input}'.";
         }
 
-        internal void MultiFilterVehicle()
+        internal FilterData MultiFilterVehicle()
         {
             string multiFilterPrompt = "\nYou may filter the vehicle list by specific key words." +
     "\nSpecify [color] [mobility] [type] [n wheels] as desired in any order." +
@@ -120,10 +121,10 @@ namespace The_Garage_Exercise.Managers
                 ListSingleVehicle(vehicle);
             }
 
-
+            return data;
         }
 
-        internal void ParkVehicle()
+        internal string ParkVehicle()
         {
             Console.WriteLine("\nYou have chosen to park your vehicle.");
 
@@ -133,7 +134,7 @@ namespace The_Garage_Exercise.Managers
             {
                 Console.WriteLine("\nApologies, but there are no free parking spots avilable currently. " +
                     "\nPlease try another garage.");
-                return;
+                return "No parking.";
             }
 
             Console.WriteLine($"\nA parking spot is available.");
@@ -143,17 +144,18 @@ namespace The_Garage_Exercise.Managers
             if (vehicle == null)
             {
                 Console.WriteLine("\nRegistration failed.");
-                return;
+                return "No vehicle.";
             }
 
             Handler.ParkVehicle(vehicle);
+            return $"Parked a {vehicle.Color} {vehicle.GetType().ToString()}.";
         }
 
-        internal void RetrieveVehicle()
+        internal string RetrieveVehicle()
         {
             string retrievePrompt = "\nPlease provide the license number of the vehicle you wish to retrieve: ";
 
-            Handler.RetrieveVehicle(Help.GetInput(retrievePrompt));
+            return Handler.RetrieveVehicle(Help.GetInput(retrievePrompt));
         }
 
 
