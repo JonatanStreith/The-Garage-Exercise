64b47af0415cec92028040e20e7888f566146f3a
Jonatan Streith <jonatan@streith.se>
2026-05-26 08:34:35 +0200

    Created GarageManagerTest.

---
+++ The Garage Exercise.Test/GarageManagerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Garage_Exercise.Test
+{
+    public class GarageManagerTest
+    {
+        [Fact]
+        public void CanSizeGarage()
+        {
+
+        }
+
+
+    }
+}
